577a146d129f9950d9dc64e728cbfec628ff2c59
Benjamin Audet <benjamin.audet@epitech.eu>
2016-10-18 14:47:11 +0200

    sdf

--- js/grayscale.js
+++ js/grayscale.js
@@ -37,7 +37,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(45.7579502, 4.8001017));
 });
 
 function init() {
