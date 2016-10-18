93544b0863aabc736a110295dc97117ac69013e9
Benjamin Audet <benjamin.audet@epitech.eu>
2016-10-18 14:48:18 +0200

    qsdf

--- js/grayscale.js
+++ js/grayscale.js
@@ -48,7 +48,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(45.7579502, 4.8001017), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
